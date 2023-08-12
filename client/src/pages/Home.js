75737ac3b17c5a865ca132422b36901e9c009410
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-08-12 09:31:52 +0530

    added styles to cards

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Input from "../components/cards/Input";
+import Profile from "../components/cards/Profile";
+import List from "../components/cards/List";
 
 const Home = () => {
-  return <div className="container">Home</div>;
+  return (
+    <div className="container">
+      <div className="dashboard">
+        <Input />
+        <Profile />
+      </div>
+      <div className="list">
+        <List />
+        <List />
+        <List />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
