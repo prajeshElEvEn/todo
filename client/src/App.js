3dd2da61a2e3c7c21104f00ecb0ab29e55beabca
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-10-06 23:18:14 +0530

    added new client

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
-import { Route, Routes } from "react-router-dom";
-import "./styles/App.css";
-import Home from "./pages/Home";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- client/src/components/cards/List.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const List = () => {
-  return <div className="card">List</div>;
-};
-
-export default List;
