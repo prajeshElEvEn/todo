8907bc7dbde034195e3acf068a3fd51fad2f9b39
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-10-15 22:46:12 +0530

    added index

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const { env, db, warn, log } = require("./server/utils");
+const constants = require("./constants");
+const { errorHandler } = require("./server/middlewares/errorMiddlewares");
+const { auth, user } = require("./server/routes");
+
+function startServer() {
+  const currentEnv = env();
+  const port = process.env.PORT || 5000;
+  const hostname = process.env.HOSTNAME || "127.0.0.1";
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use("/uploads", express.static(constants.paths.uploadDir));
+
+  app.use("/api/auth", auth);
+  app.use("/api/users", user);
+
+  app.use(errorHandler);
+
+  app.listen(port, hostname, async () => {
+    warn(`Current environment: ${currentEnv}`);
+    await db();
+    log(`Server running at http://${hostname}:${port}`);
+  });
+}
+
+startServer();
