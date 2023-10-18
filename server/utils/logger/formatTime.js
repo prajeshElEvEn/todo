634b76efbc03a6c0e7262ea44eaede87133bbb32
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-10-18 23:31:21 +0530

    optimized logger

--- server/utils/logger/formatTime.js
+++ server/utils/logger/formatTime.js
@@ -1,15 +1,14 @@
-// formatTime.js
 const chalk = require("chalk");
 
 const formatTime = () => {
-  var date = new Date();
-  var hour = date.getHours();
-  var min = date.getMinutes();
-  var sec = date.getSeconds();
-  var day = date.getDate();
-  var mon = date.getMonth();
-  var year = date.getFullYear();
-  time = chalk.whiteBright.bold(
+  const date = new Date();
+  const hour = date.getHours();
+  const min = date.getMinutes();
+  const sec = date.getSeconds();
+  const day = date.getDate();
+  const mon = date.getMonth();
+  const year = date.getFullYear();
+  const time = chalk.whiteBright.bold(
     `[${day}-${mon}-${year} | ${hour}:${min}:${sec}]`
   );
   return time;
--- server/utils/logger/index.js
+++ server/utils/logger/index.js
@@ -1,37 +1,34 @@
 const chalk = require("chalk");
 const formatTime = require("./formatTime");
 
-const log = (message) => {
-  var time = formatTime();
-  var pre = chalk.bgWhiteBright.bold("[LOG]");
-  message = chalk.whiteBright(message);
-  console.log(`${time} ${pre} ${message}`);
+const logType = (type, message, color) => {
+  const time = formatTime();
+  let coloredMessage;
+
+  switch (color) {
+    case "WhiteBright":
+      coloredMessage = chalk.whiteBright(message);
+      break;
+    case "Green":
+      coloredMessage = chalk.green(message);
+      break;
+    case "Red":
+      coloredMessage = chalk.red(message);
+      break;
+    case "Yellow":
+      coloredMessage = chalk.yellow(message);
+      break;
+    default:
+      coloredMessage = message;
+  }
+
+  const prefix = chalk["bg" + color].bold(`[${type.toUpperCase()}]`);
+  console.log(`${time} ${prefix} ${coloredMessage}`);
 };
 
-const success = (message) => {
-  var time = formatTime();
-  var pre = chalk.bgGreen.bold("[SUCCESS]");
-  message = chalk.green(message);
-  console.log(`${time} ${pre} ${message}`);
-};
+const log = (message) => logType("log", message, "WhiteBright");
+const success = (message) => logType("success", message, "Green");
+const error = (message) => logType("error", message, "Red");
+const warn = (message) => logType("warn", message, "Yellow");
 
-const error = (message) => {
-  var time = formatTime();
-  var pre = chalk.bgRed.bold("[ERROR]");
-  message = chalk.red(message);
-  console.log(`${time} ${pre} ${message}`);
-};
-
-const warn = (message) => {
-  var time = formatTime();
-  pre = chalk.bgYellow.bold("[WARN]");
-  message = chalk.yellow(message);
-  console.log(`${time} ${pre} ${message}`);
-};
-
-module.exports = {
-  log,
-  success,
-  error,
-  warn,
-};
+module.exports = { log, success, error, warn };
