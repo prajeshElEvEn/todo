eccb38e9dc3930c37cd8df2b542a4719b738a611
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-10-23 12:36:58 +0530

    added todo controllers

--- server/controllers/todoControllers.js
+++ server/controllers/todoControllers.js
@@ -10,10 +10,10 @@ const getTodos = asyncHandler(async (req, res) => {
 });
 
 // @desc    Gets user todos
-// @route   GET /api/todos/:id
+// @route   GET /api/todos/:userId
 // @access  Private
 const getUserTodos = asyncHandler(async (req, res) => {
-  const userId = req.params.id;
+  const userId = req.params.userId;
   const todos = await Todo.find({ author: userId });
   res.status(200).json(todos);
 });
@@ -26,3 +26,83 @@ const getTodo = asyncHandler(async (req, res) => {
   const todo = await Todo.findById(todoId);
   res.status(200).json(todo);
 });
+
+// @desc    Creates a todo
+// @route   POST /api/todos/
+// @access  Private
+const createTodo = asyncHandler(async (req, res) => {
+  const { title, author } = req.body;
+
+  if (!title) {
+    res.status(404);
+    throw new Error("please fill in all the fields");
+  }
+
+  if (!author) {
+    res.status(404);
+    throw new Error("author not found");
+  }
+
+  const todo = new Todo({
+    title,
+    author,
+  });
+
+  await todo.save();
+
+  return res.status(201).json(todo);
+});
+
+// @desc    Updates a todo
+// @route   PUT /api/todos/
+// @access  Private
+const updateTodo = asyncHandler(async (req, res) => {
+  const todoId = req.params.id;
+  const { title } = req.body;
+
+  const todo = await Todo.findById(todoId);
+
+  if (!todo) {
+    res.status(404);
+    throw new Error("todo not found");
+  }
+
+  if (title) {
+    todo.title = title;
+  }
+
+  const updatedTodo = await todo.save();
+
+  res.status(200).json(updatedTodo);
+});
+
+// @desc    Deletes a todo
+// @route   DELETE /api/todos/
+// @access  Private
+const deleteTodo = asyncHandler(async (req, res) => {
+  const todoId = req.params.id;
+
+  const todo = await Todo.findById(todoId);
+
+  if (!todo) {
+    res.status(404);
+    throw new Error("todo not found");
+  }
+
+  if (title) {
+    poster.title = title;
+  }
+
+  await poster.deleteOne();
+
+  res.status(204).json({ id: poster._id, message: "Poster deleted" });
+});
+
+module.exports = {
+  getTodos,
+  getUserTodos,
+  getTodo,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+};
--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const { checkAuth, checkAdmin } = require("../middlewares/authMiddlewares");
+const {
+  getTodos,
+  createTodo,
+  getUserTodos,
+  getTodo,
+  updateTodo,
+  deleteTodo,
+} = require("../controllers/todoControllers");
 const router = express.Router();
 
-router.route("/").get(checkAuth, checkAdmin).post(checkAuth);
+router
+  .route("/")
+  .get(checkAuth, checkAdmin, getTodos)
+  .post(checkAuth, createTodo);
+
+router.route("/:userId").get(checkAuth, getUserTodos);
+
 router
   .route("/:id")
-  .get(checkAuth)
-  .get(checkAuth)
-  .put(checkAuth)
-  .delete(checkAuth);
+  .get(checkAuth, getTodo)
+  .put(checkAuth, updateTodo)
+  .delete(checkAuth, deleteTodo);
 
 module.exports = router;
