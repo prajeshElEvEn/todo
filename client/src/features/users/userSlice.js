a9fc8476c57e252562602ffb2c95c75e8adec654
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-11-20 16:47:08 +0530

    edited userslice

--- client/src/features/users/userSlice.js
+++ client/src/features/users/userSlice.js
@@ -1,11 +1,29 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  user: {},
+  user: null,
+  users: [],
   isLoading: false,
   isError: false,
   message: "",
 };
+
+export const getUsers = createAsyncThunk(
+  "user/getUsers",
+  async (userData, thunkAPI) => {
+    try {
+      //   return await registerService(userData);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 export const getUser = createAsyncThunk(
   "user/getUser",
@@ -23,3 +41,93 @@ export const getUser = createAsyncThunk(
     }
   }
 );
+
+export const updateUser = createAsyncThunk(
+  "user/updateUser",
+  async (userData, thunkAPI) => {
+    try {
+      //   return await registerService(userData);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const deleteUser = createAsyncThunk(
+  "user/deleteUser",
+  async (userData, thunkAPI) => {
+    try {
+      //   return await registerService(userData);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+  extraReducers: (builders) => {
+    builders
+      .addCase(getUsers.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(getUsers.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.users = action.payload;
+        state.isError = false;
+      })
+      .addCase(getUsers.rejected, (state, action) => {
+        state.isLoading = false;
+        state.users = [];
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(getUser.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(getUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload;
+        state.isError = false;
+      })
+      .addCase(getUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(updateUser.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(updateUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        if (state.user._id === action.payload._id) {
+          state.user = action.payload;
+        }
+        state.users = state.users.map((user) =>
+          user._id === action.payload._id ? action.payload : user
+        );
+        state.isError = false;
+      })
+      .addCase(updateUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
+});
